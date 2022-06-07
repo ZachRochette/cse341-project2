73c8eb6f1541ed3d3d1aa45336f3939ba993bc04
ZachRochette <zachrochette@hotmail.com>
2022-06-07 01:38:28 -0600

    commit

--- models/users.js
+++ models/users.js
@@ -1,28 +1,67 @@
 const mongoose = require('mongoose');
 
 // create schema and model
-const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    displayName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    profilePicture: {
+      type: String,
+      default: ''
+    },
+    coverPicture: {
+      type: String,
+      default: ''
+    },
+    followers: {
+      type: Array,
+      default: []
+    },
+    following: {
+      type: Array,
+      default: []
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    },
+    desc: {
+      type: String,
+      max: 50
+    },
+    city: {
+      type: String,
+      max: 50
+    },
+    from: {
+      type: String,
+      max: 50
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3]
+    }
   },
-  displayName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  phoneNumber: {
-    type: String,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 const UsersModel = mongoose.model('users', UserSchema);
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const UsersModel = require('../models/users');
+const bcrypt = require('bcrypt');
 
 // GET ALL USER
 routes.get('/', (req, res) => {
@@ -46,6 +47,44 @@ routes.delete('/:id', (req, res) => {
   UsersModel.deleteOne({ _id: req.params.id }, (err, result) => {
     res.json(result);
   });
+});
+
+// Login and Register
+//REGISTER
+routes.post('/', async (req, res) => {
+  try {
+    // generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create new user
+    const newUser = new user({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+routes.post('/login', async (req, res) => {
+  try {
+    const user = await user.findOne({ email: req.body.email });
+    !user && res.status(404).send('user not found');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json('Wrong Password');
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = routes;
