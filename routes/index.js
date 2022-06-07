6c59281b1fd0071d688c7638e0f7186de32697bd
ZachRochette <zachrochette@hotmail.com>
2022-06-07 02:03:59 -0600

    updated index routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const routes = require('express').Router();
 
 routes.use('/', require('./swagger'));
 routes.use('/users', require('./users'));
+routes.use('/auth', require('./auth'));
+routes.use('/posts', require('./posts'));
 
 module.exports = routes;
